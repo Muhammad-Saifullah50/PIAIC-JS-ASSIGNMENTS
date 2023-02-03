415b52fc3d1e3840227f1f27248d3a8170ad3cf5
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-02-03 22:31:55 +0500

    completd chap 9 assignment

---
+++ chap9.js
@@ -0,0 +1,7 @@
+var firstname = prompt("Enter your firstname ");
+
+var lastname = prompt("Enter your lastname");
+
+var age = prompt("Enter your age");
+
+alert("Hello " + firstname + lastname + " You are " + age + " years old");
