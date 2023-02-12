79e34024631e9934eddaa71c1924efd2a64ba18f
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-02-12 21:14:46 +0500

    updated assignments

---
+++ chapter-10-14/chap-10-14.js
@@ -0,0 +1,10 @@
+// Calculator
+
+let value1 = prompt("Enter a number")
+let operater = prompt("Enter an operater")
+let value2 = prompt("Enter second number")
+
+if(operater === "+") {
+    let sum = value1 + value2;
+    alert(sum);
+}
