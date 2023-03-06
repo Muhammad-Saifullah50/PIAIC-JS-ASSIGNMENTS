03e91ea1ebc052642f648309029fb3f21d25712b
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-03-06 20:03:31 +0500

    some bug fix remainig

---
+++ chapter-18,19,20/chapter-18-20.js
@@ -0,0 +1,16 @@
+let courses = [];
+
+for (let i = 1; i <= 5; i++) {
+    let courseName = prompt(`Enter course ${i} name`, "");
+    courses.push(courseName)
+}
+
+alert(courses)
+
+for (let i = 1; i = courses.length; i++) {
+    let courseNameEdit = prompt(`Edit course ${i} name`, "");
+    courses.splice(i, 1, courseNameEdit);
+
+}
+
+alert(courses)
